8861137289693ab06110c826438c1ab3162abbac
EsmaeiliSina <es.sina76@gmail.com>
2021-02-15 11:48:51 +0330

    init commit

---
+++ android/app/src/main/kotlin/com/example/hirad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hirad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Text("Hirad"),
+        ),
+      ),
+    );
+  }
+}
