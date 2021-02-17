957c9e8f37e25bbcac2fba96a0a8ac7fdb9d88eb
EsmaeiliSina <es.sina76@gmail.com>
2021-02-17 13:52:59 +0330

    add 7 flat button

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,72 @@ class MyApp extends StatelessWidget {
                   width: double.infinity,
                   color: Colors.red,
                 ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                child: Container(
+                  height: 20,
+                  width: double.infinity,
+                  color: Colors.orange,
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+                child: Container(
+                  height: 20,
+                  width: double.infinity,
+                  color: Colors.yellow,
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+                child: Container(
+                  height: 20,
+                  width: double.infinity,
+                  color: Colors.green,
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+                child: Container(
+                  height: 20,
+                  width: double.infinity,
+                  color: Colors.green[900],
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+                child: Container(
+                  height: 20,
+                  width: double.infinity,
+                  color: Colors.blue,
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+                child: Container(
+                  height: 20,
+                  width: double.infinity,
+                  color: Colors.purple,
+                ),
               )
             ],
           ),
