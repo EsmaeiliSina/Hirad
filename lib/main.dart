2174591f733267ae52e65ec4884f9fd7c8716ffc
EsmaeiliSina <es.sina76@gmail.com>
2021-02-17 13:04:40 +0330

    change app bar title

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(
             title: Text(
-              "هیراد",
+              "بلز هیراد",
             ),
           ),
         ),
